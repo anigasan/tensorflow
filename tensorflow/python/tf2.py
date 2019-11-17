d5746bdeef6cbfe2469f16b903d475b2df76bc80
Ananya Gangavarapu <anigasan637@gmail.com>
2019-11-17 18:18:33 -0500

    Update tf2.py

--- tensorflow/python/tf2.py
+++ tensorflow/python/tf2.py
@@ -17,7 +17,7 @@
 This module is meant for TensorFlow internal implementation, not for users of
 the TensorFlow library. For that see tf.compat instead.
 """
-
+#update
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
